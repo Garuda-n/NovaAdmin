f374a5e831e9e110c9302f845f66469d6f3fbf8f
Gokul <gokulandtech@gmail.com>
2026-06-29 00:04:45 +0530

    Initial NovaAdmin setup

---
+++ resources/views/home.blade.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>NovaAdmin</title>
+</head>
+<body>
+
+    <h1>🚀 NovaAdmin Dashboard</h1>
+
+    <hr>
+
+    <h3>Project Status</h3>
+
+    <ul>
+        <li>✅ Laravel Connected</li>
+        <li>✅ MySQL Connected</li>
+        <li>✅ Development Started</li>
+    </ul>
+
+</body>
+</html>
